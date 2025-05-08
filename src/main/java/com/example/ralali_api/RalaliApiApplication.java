480d20c4c3c59edebb3fdbfcb9ca550151f131c8
chean wei jie <cheanweijie02@gmail.com>
2025-05-08 11:09:36 +0700

    test

---
+++ src/main/java/com/example/ralali_api/RalaliApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.ralali_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RalaliApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RalaliApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ralali_api/RalaliApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ralali_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RalaliApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
